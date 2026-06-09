0c285eeaacdcf580fb1a525b37d735567bec7b7a
XJINE <xjine.andorelseif@gmail.com>
2026-06-09 13:42:08 +0900

    First commit.

---
+++ Assets/Packages/KeyEventManager/KeyEventManager.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class KeyEventManager : SingletonMonoBehaviour<KeyEventManager>
+{
+    public enum KeyInputType
+    {
+        WasPressedThisFrame,
+        WasReleasedThisFrame,
+        IsPressed,
+    }
+
+    [System.Serializable]
+    public class KeyEvent
+    {
+        public Key          key;
+        public KeyInputType inputType;
+        public UnityEvent   handler;
+    }
+
+    [field: SerializeField] public List<KeyEvent> KeyEvents { get; private set; }
+
+    private void Update()
+    {
+        var keyboard = Keyboard.current;
+
+        if (keyboard == null)
+        {
+            return;
+        }
+
+        foreach (var keyEvent in from keyEvent in KeyEvents let satisfied = keyEvent.inputType switch
+        {
+            KeyInputType.WasPressedThisFrame  => keyboard[keyEvent.key].wasPressedThisFrame,
+            KeyInputType.WasReleasedThisFrame => keyboard[keyEvent.key].wasReleasedThisFrame,
+            KeyInputType.IsPressed            => keyboard[keyEvent.key].isPressed,
+            _                                 => false
+        } where satisfied select keyEvent)
+        {
+            keyEvent.handler.Invoke();
+        }
+    }
+
+    public bool TryGetValue(Key key, out KeyEvent keyEvent)
+    {
+        keyEvent = null;
+
+        foreach (var e in KeyEvents.Where(e => e.key == key))
+        {
+            keyEvent = e;
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Unity_KeyEventManager
+
+<img src="https://github.com/XJINE/Unity_KeyEventManager/blob/main/Screenshot.png" width="100%" height="auto" />
+
+KeyEventManager provides a key-based event handling system.
+
+## Importing
+
+You can use Package Manager or import it directly.
+
+```
+https://github.com/XJINE/Unity_KeyEventManager.git?path=Assets/Packages/KeyEventManager
+```
+
+### Dependencies
+
+This project use following resources.
+
+- https://github.com/XJINE/Unity_SingletonMonoBehaviour
+- Unity.InputSystem
+
+## How to Use
+
+Register your key and function pairs in the Inspector.
